3f985bbdb61c37ee8f2c392f9eefd1a02f5f0a72
Sandy <sav3ae@virginia.edu>
2018-06-16 17:08:23 -0400

    Completed signup page

--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -44,58 +44,60 @@ class Signup extends React.Component {
 
   render() {
     return (
-      <div className="signup-form-div">
-        <h1>Instapix</h1>
-        <form className="signup-form">
-          <h2>Sign up and see photos and videos from your friends.</h2>
-          <div className="demo-login">
-            <button>Sign in as Demo User</button>
-          </div>
-          <div className="or-label">OR</div>
-          <label>
-            <input
-              type="text"
-              value={this.state.email}
-              onChange={this.handleInput('email')}
-              placeholder="Email"
-            />
-          </label>
-          <br/>
-          <label>
-            <input
-              type="text"
-              value={this.state.name}
-              onChange={this.handleInput('name')}
-              placeholder="Name"
-            />
-          </label>
-          <br/>
-          <label>
-            <input
-              type="text"
-              value={this.state.username}
-              onChange={this.handleInput('username')}
-              placeholder="Username"
-            />
-          </label>
-          <br/>
-          <label>
-            <input
-              type="password"
-              value={this.state.password}
-              onChange={this.handleInput('password')}
-              placeholder="Password"
-            />
-          </label>
-          <br/>
-          <button className="signup-btn" onClick={this.handleSubmit}>Sign Up</button>
-          {this.renderErrors()}
-          <div className="login-link">
-            <p>Have an account? <Link to="/login">Log in</Link></p>
-          </div>
-        </form>
-        
+      <div className="signup-div">
+        <div className="signup-form-div">
+          <form className="signup-form">
+            <h1>Insapix</h1>
+            <h2>Sign up and see photos and videos from your friends.</h2>
+            <div className="demo-login">
+              <button>Sign in as Demo User</button>
+            </div>
+            <div className="or-label">OR</div>
+            <label>
+              <input
+                type="text"
+                value={this.state.email}
+                onChange={this.handleInput('email')}
+                placeholder="Email"
+              />
+            </label>
+            <br/>
+            <label>
+              <input
+                type="text"
+                value={this.state.name}
+                onChange={this.handleInput('name')}
+                placeholder="Name"
+              />
+            </label>
+            <br/>
+            <label>
+              <input
+                type="text"
+                value={this.state.username}
+                onChange={this.handleInput('username')}
+                placeholder="Username"
+              />
+            </label>
+            <br/>
+            <label>
+              <input
+                type="password"
+                value={this.state.password}
+                onChange={this.handleInput('password')}
+                placeholder="Password"
+              />
+            </label>
+            <br/>
+            <button className="signup-btn" onClick={this.handleSubmit}>Sign Up</button>
+            {this.renderErrors()}
+          </form>
+        </div>
+        <div className="login-link">
+          <p>Have an account? <Link to="/login">Log in</Link></p>
+        </div>
       </div>
+
     );
   }
 }
